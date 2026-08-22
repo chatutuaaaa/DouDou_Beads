e46fdb739e742fd590e7cc8db4d83fe463bd990c
chatutuaaaa <chatutuaaaa@gmail.com>
2026-08-22 20:12:29 +0800

    Use cloud hosting service config

--- utils/config.js
+++ utils/config.js
@@ -1,8 +1,8 @@
 // 本地开发：保持 apiBaseUrl，cloudEnv 留空。
 // 微信云托管：填入环境 ID 和服务名后，请求走 wx.cloud.callContainer。
 const apiBaseUrl = 'http://127.0.0.1:5001'
-const cloudEnv = ''
-const cloudService = 'doudoutu'
+const cloudEnv = 'prod-d4gg40zm9356c7c07'
+const cloudService = 'flask-0sno'
 
 module.exports = {
   apiBaseUrl,
